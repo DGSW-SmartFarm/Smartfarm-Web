95bdf7db0bbeb3147c6add6eb41e04c70ba8b293
2001hcy <2001.hcy@gmail.com>
2018-12-18 23:16:02 +0900

    웹화면 구조 완성

--- frontend/src/components/ShowPage.vue
+++ frontend/src/components/ShowPage.vue
@@ -1,6 +1,38 @@
 <template>
   <div>
-    {{movie}}
+    <div style="float: left; width: 75%;">
+      <div class="sensor_panel">
+        <div class="panel_icons"><img src="../../img/temperature.png"></div>
+        <div class="panel_content">
+          <p class="content_title">온도</p>  
+          <p class="content_value">{{data.t}}<span class="content_unit"> °C</span></p>
+        </div>
+      </div >
+      <div class="sensor_panel">
+        <div class="panel_icons"><img src="../../img/humidity.png"></div>
+        <div class="panel_content">
+          <p class="content_title">습도</p>  
+          <p class="content_value">{{data.h}}<span class="content_unit"> %</span></p>
+          </div>
+      </div>
+      <div class="sensor_panel">
+        <div class="panel_icons"><img src="../../img/co2.png"></div>
+        <div class="panel_content">
+          <p class="content_title">CO<span style="font-size:50%">2</span></p>  
+          <p class="content_value">{{data.c}}<span class="content_unit"> ㎍/㎥</span></p>
+          </div>
+      </div>
+      <div class="sensor_panel">
+        <div class="panel_icons"><img src="../../img/brightness.png"></div>
+        <div class="panel_content">
+          <p class="content_title">조도</p>  
+          <p class="content_value">{{data.i}}<span class="content_unit"> lx</span></p>
+          </div>
+      </div>
+    </div>
+    <div style="float: left; width: 25%">
+        <h1>DGSW-Smartfarm</h1>
+    </div>
   </div>
 </template>
 
@@ -11,17 +43,41 @@ export default {
     // 갱신하고나서 불러옴
     this.$http.get(`/api/get`)
     .then((response) => {
-      this.movie = response.data
+      this.data = response.data
     })
   },
   data: function () {
     return {
-      movie: {}
+      data: {}
     }
   }
 }
 </script>
 
 <style lang="css">
-
+  @import url(//fonts.googleapis.com/earlyaccess/nanumgothic.css);
+  body{margin: 0 0 0 0}
+  .sensor_panel{
+    float: left; 
+    width: 48%;
+    border:2px solid gray; 
+    border-radius: 10px;
+    margin:1% 0% 0% 1%;
+  }
+  .content_title{
+    font-family: 'Nanum Gothic', sans-serif;
+    font-size: 250%;
+    font-weight: 400;
+  }
+  .content_value{
+    font-family: 'Nanum Gothic', sans-serif;
+    font-size: 350%;
+    font-weight: 400; 
+  }
+  .content_unit{font-size: 50%}
+  .panel_icons{
+    float: left; 
+    width: 50%;
+    margin: 10px 0px 10px 10px;
+  }
 </style>
